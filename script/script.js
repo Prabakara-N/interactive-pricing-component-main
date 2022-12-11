6968790a6680bed25311397a9518d117dd798fc4
prabakaran <mspk028@gmail.com>
2022-12-11 22:29:01 +0530

    css edited

--- script/script.js
+++ script/script.js
@@ -1,58 +1,17 @@
-// // get elements
-
-// const pageViewEl = document.getElementById("views");
-// const dollarEl = document.getElementById("dollar");
-// const rangeEl = document.getElementById("range-input");
-// const yearBillEl = document.getElementById("checkbox");
-
-// const startBtn = document.querySelector(".btn");
-
-// // global variables
-
-// // page views in array
-// let pageViews = ["10K", "50K", "100K", "500K", "1M"];
-// // perMonths in array
-// let perMonth = [8, 12, 16, 24, 36];
-// // yearly
-// let yearly = false;
-
-// // event listeners
-// rangeEl.addEventListener("input", function () {
-//   updateValue();
-//   pageViewEl.innerHTML = `${pageViews[rangeEl.value]} PAGE VIEWS`;
-//   dollarEl.innerHTML = `$${perMonth[rangeEl.value]}/month`;
-// });
-
-// // yearly billing
-
-// yearBillEl.addEventListener("change", function () {
-//   if (yearly == false) {
-//     yearly = true;
-//   } else {
-//     yearly = false;
-//   }
-//   updateValue();
-// });
-
-// // update value 25% discount
-
-// function updateValue() {
-//   if (yearly) {
-//     dollarEl.innerText = `$${perMonth[rangeEl.value] * 0.75} /month`;
-//   } else {
-//     dollarEl.innerText = `$${perMonth[rangeEl.value]} /month`;
-//   }
-// }
-
+//elements
 let viewEl = document.getElementById("views");
 let costEl = document.getElementById("dollar");
 let sliderEl = document.getElementById("price-slider");
 let toggleEl = document.getElementById("billing");
 
+//global variables
 let pageViews = ["10K", "50K", "100K", "500K", "1M"];
 let perMonth = [8, 12, 16, 24, 36];
 let isYearly = false;
 
+//event listneres
+
+//for slider input
 sliderEl.addEventListener("input", function () {
   updateValue();
   viewEl.innerHTML = pageViews[sliderEl.value];
@@ -66,6 +25,7 @@ sliderEl.addEventListener("input", function () {
             hsl(224, 65%, 95%) 100%)`;
 });
 
+//for toggle input
 toggleEl.addEventListener("change", function () {
   if (isYearly == false) {
     isYearly = true;
@@ -75,6 +35,7 @@ toggleEl.addEventListener("change", function () {
   updateValue();
 });
 
+//functions
 function updateValue() {
   if (isYearly) {
     costEl.innerHTML = perMonth[sliderEl.value] * 0.75;
