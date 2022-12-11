e64ef7450d0d32c7c156e42803ddd1d15daafaed
prabakaran <mspk028@gmail.com>
2022-12-11 18:26:13 +0530

    js added

--- script/script.js
+++ script/script.js
@@ -1,1 +1,45 @@
+// get elements
 
+const pageViewEl = document.getElementById("views");
+const dollarEl = document.getElementById("dollar");
+const rangeEl = document.getElementById("range-input");
+const yearBillEl = document.getElementById("checkbox");
+
+const startBtn = document.querySelector(".btn");
+
+// global variables
+
+// page views in array
+let pageViews = ["10K", "50K", "100K", "500K", "1M"];
+// perMonths in array
+let perMonth = [8, 12, 16, 24, 36];
+// yearly
+let yearly = false;
+
+// event listeners
+rangeEl.addEventListener("input", function () {
+  updateValue();
+  pageViewEl.innerText = `${pageViews[rangeEl.value]} PAGE VIEWS`;
+  dollarEl.innerText = `$${perMonth[rangeEl.value]}/month`;
+});
+
+// yearly billing
+
+yearBillEl.addEventListener("change", function () {
+  if (yearly == false) {
+    yearly = true;
+  } else {
+    yearly = false;
+  }
+  updateValue();
+});
+
+// update value 25% discount
+
+function updateValue() {
+  if (yearly) {
+    dollarEl.innerText = perMonth[rangeEl.value] * 0.75;
+  } else {
+    dollarEl.innerText = perMonth[rangeEl.value];
+  }
+}
